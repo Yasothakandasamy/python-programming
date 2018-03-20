fa0a315f4d78c7b88f48c4249fe682e781c7c971
Yasothakandasamy <37342876+Yasothakandasamy@users.noreply.github.com>
2018-03-20 09:56:11 +0530

    Create largest of three numbers.py

---
+++ beginner level/largest of three numbers.py
@@ -0,0 +1,10 @@
+a=raw_input()
+b=raw_input()
+c=raw_input()
+if(a>b and a>c):
+    print("a is greater")
+if(b>a and b>c):
+    print("b is greater")
+if(c>a and c>b):
+    print("c is greater")
+		
